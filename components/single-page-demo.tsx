923afb307c7e64fc1cf11d61222dcf760d263162
Ryan Gaus <rsg1egoman@gmail.com>
2025-09-02 10:58:54 -0400

    feat: get rid of conditional property accesses, for the most part

--- components/single-page-demo.tsx
+++ components/single-page-demo.tsx
@@ -48,34 +48,30 @@ export default function SinglePageDemo() {
       <div>
         Statuses: {agentSession.connectionState} / {agentSession.agent?.conversationalState ?? 'N/A'}
       </div>
-      <Button variant="primary" onClick={() => setStarted(s => !s)}>
+      <Button variant="primary" onClick={() => setStarted(s => !s)} disabled={agentSession.connectionState === 'connecting'}>
         {agentSession.isConnected ? 'Disconnect' : 'Connect'}
       </Button>
 
       {agentSession.isConnected ? (
         <div>
-          {agentSession.local ? (
-            <div>
-              <Button onClick={() => agentSession.local.camera?.toggle?.()}>
-                {agentSession.local.camera?.enabled ? 'Disable' : 'Enable'} local camera
-              </Button>
-              <Button onClick={() => agentSession.local?.microphone?.toggle?.()}>
-                {agentSession.local.microphone?.enabled ? 'Mute' : 'Un mute'} local microphone
-              </Button>
-              <div>
-                <p>Local microphone sources:</p>
-                {agentSession.local.microphone?.devices.list.map(item => (
-                  <li
-                    key={item.deviceId}
-                    onClick={() => agentSession.local.microphone?.devices.changeActive(item.deviceId)}
-                    style={{ color: item.deviceId === agentSession?.local.microphone?.devices.activeId ? 'red' : undefined }}
-                  >
-                    {item.label}
-                  </li>
-                ))}
-              </div>
-            </div>
-          ) : null}
+          <Button onClick={() => agentSession.local.camera?.toggle?.()}>
+            {agentSession.local.camera.enabled ? 'Disable' : 'Enable'} local camera
+          </Button>
+          <Button onClick={() => agentSession.local?.microphone?.toggle?.()}>
+            {agentSession.local.microphone.enabled ? 'Mute' : 'Un mute'} local microphone
+          </Button>
+          <div>
+            <p>Local microphone sources:</p>
+            {agentSession.local.microphone.devices.list.map(item => (
+              <li
+                key={item.deviceId}
+                onClick={() => agentSession.local.microphone.devices.changeActive(item.deviceId)}
+                style={{ color: item.deviceId === agentSession.local.microphone.devices.activeId ? 'red' : undefined }}
+              >
+                {item.label}
+              </li>
+            ))}
+          </div>
 
           {agentSession.local.camera ? (
             <AgentVideoTrack track={agentSession.local.camera} />
@@ -85,7 +81,7 @@ export default function SinglePageDemo() {
           ) : null}
 
           <ul>
-            {agentSession.messages?.list.map(message => (
+            {agentSession.messages.list.map(message => (
               <li key={message.id}>{message.content.text}</li>
             ))}
             <li>
@@ -95,7 +91,7 @@ export default function SinglePageDemo() {
                 onChange={e => setChatMessage(e.target.value)}
               />
               <Button onClick={() => {
-                agentSession.messages?.send(chatMessage);
+                agentSession.messages.send(chatMessage);
                 setChatMessage('');
               }}>Send</Button>
             </li>
